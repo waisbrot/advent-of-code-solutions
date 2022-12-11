a549b0cf11dec3291b6884126a41319f2496a735
Nathaniel Waisbrot <code@waisbrot.net>
2022-12-11 10:39:40 -0500

    day 10 working

--- erlang/src/aoc_problem_10.erl
+++ erlang/src/aoc_problem_10.erl
@@ -15,7 +15,7 @@ ingest(Problem, Example) ->
 
 solve_1(Instructions) ->
     % io:format("Instructions=~p~n", [Instructions]),
-    execute_and_accumulate(Instructions, 0, 1, [20, 60, 100, 140, 180, 220], 0).
+    execute_and_accumulate(Instructions, 1, 1, [20, 60, 100, 140, 180, 220], 0).
 
 execute_and_accumulate(_, _, _, [], Accum) ->
     Accum;
@@ -28,11 +28,30 @@ execute_and_accumulate([{addx, N}|Instructions], Cycle, X, [KeyCycle|Rest], Accu
     io:format("addx ~p. Signal at cycle ~p (~p): ~p (~p * ~p)~n", [N, KeyCycle, Cycle, Signal, KeyCycle, X]),
     execute_and_accumulate(Instructions, Cycle+2, X+N, Rest, Accum + Signal);
 execute_and_accumulate([noop|Instructions], Cycle, X, KeyCycles, Accum) ->
-    % io:format("~p: X=~p: noop~n", [Cycle, X]),
+    io:format("~p: X=~p: noop~n", [Cycle, X]),
     execute_and_accumulate(Instructions, Cycle+1, X, KeyCycles, Accum);
 execute_and_accumulate([{addx, N}|Instructions], Cycle, X, KeyCycles, Accum) ->
-    % io:format("~p: X=~p: addx ~p~n", [Cycle, X, N]),
+    io:format("~p: X=~p: addx ~p~n", [Cycle, X, N]),
     execute_and_accumulate(Instructions, Cycle+2, X+N, KeyCycles, Accum).
 
 solve_2(Instructions) ->
-    ok.
+    ExpandedInstructions = lists:flatmap(fun (noop) -> [noop]; (Addx) -> [noop, Addx] end, Instructions),
+    io:format("~n"),
+    execute_and_draw(ExpandedInstructions, 1, 1),
+    io:format("~n").
+
+execute_and_draw([], _, _) ->
+    ok;
+execute_and_draw([Op|Instructions], Cycle, X) ->
+    Col = Cycle rem 40,
+    if Col =:= 1 -> io:format("~n"); true -> ok end,
+    if
+        X =:= Col -> io:format("#");
+        X+1 =:= Col -> io:format("#");
+        X+2 =:= Col -> io:format("#");
+        true -> io:format(".")
+    end,
+    execute_and_draw(Instructions, Cycle+1, expanded_exec(Op, X)).
+
+expanded_exec(noop, X) -> X;
+expanded_exec({addx, N}, X) -> X+N.
