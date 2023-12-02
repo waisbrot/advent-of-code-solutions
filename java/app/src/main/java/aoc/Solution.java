b69aa89e2b90117bdd699aed611c04ab9aebdcc2
Nathaniel Waisbrot <code@waisbrot.net>
2023-12-01 19:53:25 -0500

    remove last years

--- java/app/src/main/java/aoc/Solution.java
+++
@@ -1,9 +0,0 @@
-package aoc;
-
-import java.io.IOException;
-
-public interface Solution {
-    void loadInput(boolean example) throws IOException;
-    String solution1();
-    String solution2();
-}
